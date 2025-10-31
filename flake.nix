1190a3a3151da03f5931f84852e1821f375b5948
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-10-31 23:13:12 +0100

    package with git

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Hello World Firefox addon with custom buildFirefoxXpiAddon";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+      pname = "color-picker";
+      src = ./.;
+
+      manifest = builtins.fromJSON (builtins.readFile "${src}/manifest.json");
+
+      version = manifest.version;
+      addonId = manifest.browser_specific_settings.gecko.id;
+
+      color-picker = pkgs.stdenv.mkDerivation {
+        name = "${pname}-${version}";
+        inherit src;
+
+        buildInputs = [ pkgs.zip ];
+
+        buildCommand = ''
+          dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+          mkdir -p "$dst"
+
+          cd $src
+
+          zip -r "$dst/${addonId}.xpi" *
+        '';
+
+        passthru = { inherit addonId; };
+      };
+    in
+    {
+      packages.${system} = {
+        inherit color-picker;
+        default = color-picker;
+      };
+    };
+}

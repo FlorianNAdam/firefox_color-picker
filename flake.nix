7e31ffc56e08e53731a6184f729142a0c8037167
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-11-04 20:02:39 +0100

    add signed build to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,67 @@
 {
   description = "Hello World Firefox addon with custom buildFirefoxXpiAddon";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      firefox-addons,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      lib = pkgs.lib;
 
-      pname = "color-picker";
+      nameFromString =
+        str:
+        let
+          chars = lib.strings.stringToCharacters str;
+
+          splitCamel = builtins.foldl' (
+            acc: c:
+            if acc == "" then
+              c
+            else if c >= "A" && c <= "Z" then
+              acc + "-" + lib.strings.toLower c
+            else
+              acc + lib.strings.toLower c
+          ) "" chars;
+
+          withHyphens = builtins.replaceStrings [ " " ] [ "-" ] splitCamel;
+          normalized = builtins.replaceStrings [ "--" ] [ "-" ] withHyphens;
+        in
+        normalized;
+
       src = ./.;
-
       manifest = builtins.fromJSON (builtins.readFile "${src}/manifest.json");
 
+      name = nameFromString manifest.name;
       version = manifest.version;
       addonId = manifest.browser_specific_settings.gecko.id;
 
-      color-picker = pkgs.stdenv.mkDerivation {
-        name = "${pname}-${version}";
+      color-picker = firefox-addons.lib.${pkgs.system}.buildFirefoxXpiAddon {
+        pname = name;
+        inherit version addonId;
+
+        url = "https://github.com/FlorianNAdam/firefox_color-picker/releases/download/v${version}/colorpicker.xpi";
+        sha256 = "sha256-vV9l3/ODuZZHyYQatPVudtfU0mXx2qAyZcElDh7CMhs=";
+
+        meta = {
+          description = manifest.description;
+        };
+      };
+
+      color-picker-test = pkgs.stdenv.mkDerivation {
+        name = "${name}-${version}";
         inherit src;
 
         buildInputs = [ pkgs.zip ];
@@ -29,7 +72,7 @@
 
           cd $src
 
-          zip -r "$dst/${addonId}.xpi" *
+          zip -r "$dst/${addonId}.xpi" *.js *.json
         '';
 
         passthru = { inherit addonId; };
@@ -37,7 +80,7 @@
     in
     {
       packages.${system} = {
-        inherit color-picker;
+        inherit color-picker-test color-picker;
         default = color-picker;
       };
     };
